17922078f010a8c6b770daf5ec1243a501b7df70
Think Azure Think hanu <40360909+hanu-IaaS@users.noreply.github.com>
2018-07-23 18:02:24 +0530

    Add files via upload

---
+++ iis_config.ps1
@@ -0,0 +1,23 @@
+configuration TestConfig
+{
+    Node IsWebServer
+    {
+        WindowsFeature IIS
+        {
+            Ensure               = 'Present'
+            Name                 = 'Web-Server'
+            IncludeAllSubFeature = $true
+
+        }
+    }
+
+    Node NotWebServer
+    {
+        WindowsFeature IIS
+        {
+            Ensure               = 'Absent'
+            Name                 = 'Web-Server'
+
+        }
+    }
+}
